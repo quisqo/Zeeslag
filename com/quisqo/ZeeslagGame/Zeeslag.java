988984079cc44d8dcd32a39ccc45f40093784265
quisqo <quisqo@users.noreply.github.com>
2018-01-10 13:01:11 +0100

    it giet oan

---
+++ com/quisqo/ZeeslagGame/Zeeslag.java
@@ -0,0 +1,18 @@
+package com.quisqo.ZeeslagGame;
+
+import java.util.Scanner;
+
+public class Zeeslag {
+	
+	//hier komt het board
+	//hier komt de boot
+	
+	
+	public static void main (String[] args) {
+		
+		System.out.println("De slag begint");
+		System.out.println("Breng het schip van de ander tot zinken");
+		
+	}
+
+}
