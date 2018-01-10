bcdf0f2b12aa12b0bde0709416a989cfb328cffd
quisqo <quisqo@users.noreply.github.com>
2018-01-10 19:18:29 +0100

    weer iets verder

--- com/quisqo/ZeeslagGame/Zeeslag.java
+++ com/quisqo/ZeeslagGame/Zeeslag.java
@@ -8,11 +8,26 @@ import java.util.*;
 public class Zeeslag {
 	
 	public static void main (String[] args) {
+		Scanner input = new Scanner (System.in);
 		Schip schip = new Schip();
+		String spelersSchot;
+		int aantalSchoten = 0;
+		boolean schipDrijftNog = true;
 		int [] locaties = {2, 3, 4};
 		schip.setLocaties(locaties);
-		String spelersSchot = "1";
-		schip.testSchot(spelersSchot);
+		
+		while (schipDrijftNog) {
+			String resultaat;
+			System.out.println(" Doe een schot");
+			spelersSchot = input.nextLine();
+			resultaat = schip.testSchot(spelersSchot);
+			aantalSchoten++;
+			if(resultaat.equals("Zink")) {
+				schipDrijftNog = false;
+			}
+		}
+		
+		System.out.println(" Je hebt " + aantalSchoten + " nodig gehad om het schip te laten zinken ");
 		
 	}
 		
